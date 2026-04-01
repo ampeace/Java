8190951f82430726500662a94942f4ab506064a3
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-01 15:03:01 +0530

    new fled added

---
+++ arm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class arm{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int originalnum=num;
+        int rem=0;
+        int result=0;
+
+    }
+    
+}
