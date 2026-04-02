b1a17fdaa4359a221b59a8c76f683d92029ce1f5
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-02 10:51:08 +0530

    one correction is made

--- Practice/arm.java
+++ Practice/arm.java
@@ -3,29 +3,27 @@ import java.util.*;
 public class arm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int num = sc.nextInt();
         int originalnum = num;
         int rem = 0;
         int result = 0;
 
-        // Count digits
         int n = 0;
         int temp = num;
+
         while (temp != 0) {
-            temp = temp / 10;
+            temp /= 10;
             n++;
         }
 
-        // Armstrong calculation
         temp = num;
         while (temp != 0) {
             rem = temp % 10;
-            result += (int)Math.pow(rem, n);  // Using Math.pow to calculate rem^n
-            temp = temp / 10;
+            result += (int)Math.pow(rem, n);
+            temp /= 10;
         }
 
-        // Result check
         if (result == originalnum) {
             System.out.println("Armstrong Number");
         } else {
