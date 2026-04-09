c17d52cf72a9ab6e34eeecb20d6e3a43268ce00f
ampeace <aman.kumar.singh.541@gmail.com>
2026-04-09 10:54:24 +0530

    ne Files

---
+++ Practice/swing.java
@@ -0,0 +1,25 @@
+
+// Java program using label (swing)
+// to display the message “GFG WEB Site Click”
+import java.io.*;
+import javax.swing.*;
+
+// Main class
+class swing {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        JButton button = new JButton("  WebSite Click");
+
+        button.setBounds(150, 200, 220, 50);
+
+        frame.add(button);
+
+        frame.setSize(500, 600);
+
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
+}
