5979232dfd477b1c7d67428a127c0588fd574142
ampeace <aman.kumar.singh.541@gmail.com>
2026-05-07 10:29:47 +0530

    genric array

---
+++ 01_Basics/Array/array_generic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class array_generic {
+    public static void main(String[]args){
+        ArrayList<String> List =  new ArrayList<>();
+        List.add("Aman");
+        List.add("Aman2");
+        System.out.println(List);
+
+    }
+}
---
+++ 01_Basics/Array/genric_array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class genric_array {
+    public static void main (String[]args){
+        ArrayList<Integer> list = new ArrayList<>();
+        List<String> List<Double> List2 = new ArrayList<>();
+        List<String> List3 =new ArrayList<>();
+        list.add(1);
+        list.add(2); }
+}
