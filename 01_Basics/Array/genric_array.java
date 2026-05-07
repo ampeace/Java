cc829533ebc17ba40840b0edb06835d95fc2f88b
ampeace <aman.kumar.singh.541@gmail.com>
2026-05-07 10:34:03 +0530

    new file

--- 01_Basics/Array/genric_array.java
+++ 01_Basics/Array/genric_array.java
@@ -3,8 +3,17 @@ import java.util.*;
 public class genric_array {
     public static void main (String[]args){
         ArrayList<Integer> list = new ArrayList<>();
-        List<String> List<Double> List2 = new ArrayList<>();
-        List<String> List3 =new ArrayList<>();
+        ArrayList<String> List2 = new ArrayList<>();
+        ArrayList<String> List3 =new ArrayList<>();
         list.add(1);
-        list.add(2); }
+        list.add(2);
+        List2.add("Aman");
+        List2.add("Aman2");
+        List3.add("Aman3");
+        List3.add("Aman4");
+        
+        System.out.println(list);
+        System.out.println(List2);
+        System.out.println(List3);
+    }
 }
